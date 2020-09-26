8980608c9100d8ea38ce30c866347b0aa2dcc8c6
Lucas Weber <Lucaswf659@gmail.com>
2020-09-26 19:00:58 -0300

    criação de componentes, primeira versão do header

--- gouvea-front/src/app/app.module.ts
+++ gouvea-front/src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './navegacao/footer/footer.component';
+import { HomeComponent } from './navegacao/home/home.component';
+import { MenuComponent } from './navegacao/menu/menu.component';
+import { NotFoundComponent } from './navegacao/not-found/not-found.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HomeComponent,
+    MenuComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatInputModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]

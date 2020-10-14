77bee34f9735ef49bfdc3c0d11c8c297da276ab9
Yasmin Hansen <yaa-h@hotmail.com>
2020-10-14 17:06:23 -0300

    Html do footer, adicionando free brands

--- gouvea-front/src/app/navegacao/footer/footer.component.ts
+++ gouvea-front/src/app/navegacao/footer/footer.component.ts
@@ -1,10 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faFacebook, faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html'
 })
 export class FooterComponent implements OnInit {
+  faFacebook = faFacebook;
+  faInstagram = faInstagram;
+  faWhatsapp = faWhatsapp;
+  faEnvelope = faEnvelope;
+  faMapMarkerAlt = faMapMarkerAlt;
 
   constructor() { }
 

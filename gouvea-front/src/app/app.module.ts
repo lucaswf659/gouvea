11933e9e155dec9f9a5ca1fbe5762495d6a7bf52
YasminHansen <yaa-h@hotmail.com>
2020-10-24 17:22:17 -0300

    Adiciona bootstrap

--- gouvea-front/src/app/app.module.ts
+++ gouvea-front/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -32,6 +33,7 @@ import { ProdutosComponent } from './navegacao/home/produtos/produtos.component'
   ],
   imports: [
     BrowserModule,
+    NgbModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatMenuModule,
